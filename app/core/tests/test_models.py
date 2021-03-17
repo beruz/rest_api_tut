38646271156cdf6adaef1cb6859c3598d24c95ed
Berke Eruz <beruz@users.noreply.github.com>
2021-03-17 14:29:22 +0300

    fixed errors

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,7 +6,7 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-        email = 'test@londonappdev.com'
+        email = 'test@app.com'
         password = 'Password123'
         user = get_user_model().objects.create_user(
 			email=email,
@@ -31,7 +31,7 @@ class ModelTests(TestCase):
     def test_new_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
-            'test@londonappdev.com',
+            'test@app.com',
             'test123'
         )
 
